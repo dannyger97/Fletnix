a61d27547d7ed28a07d91fd0364183d70000aaf3
Danny Gerritsen <danny_gerritsen@live.nl>
2018-01-13 10:32:57 +0100

    delete 2 meuk files

--- abonnementformulieringevuld.php
+++
@@ -1,9 +0,0 @@
-<?php
-$title= 'Abonnementformulier';
-include_once 'php/header.php';
-?>
-
-<h5>Bedankt voor het abonneren op Fletnix. Wij wensen u veel kijkplezier!</h5>
-<?php
-include_once 'php/footer.php';
-?>
