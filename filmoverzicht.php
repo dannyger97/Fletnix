52d59c8ea7820dffa7ecbca626393d81b4f02c63
Wout <w.hak99@gmail.com>
2018-01-16 14:19:47 +0100

    Filmoverzicht in ontwikkeling

--- filmoverzicht.php
+++ filmoverzicht.php
@@ -6,15 +6,20 @@ require_once 'php/dbconnectie.php';
 ?>
 
 <?php
-    $statement = "SELECT movie_id,cover_image FROM Movie";
-    $query = $dbc->prepare($statement);
-    $query->execute();
-    $i = $query->fetchAll();
-    foreach($i as $film){
-        $image=$film['cover_image'];
-        echo '<a href="filminformatie.php?movieid='.$film['movie_id'].'">
-            <img width=10% src="'.$image.'">
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $statement = "SELECT movie_id,cover_image FROM Movie";
+        $query = $dbc->prepare($statement);
+        $query->execute();
+        $i = $query->fetchAll();
+        foreach ($i as $film) {
+            $image = $film['cover_image'];
+            echo '<a href="filminformatie.php?movieid=' . $film['movie_id'] . '">
+            <img width=10% src="' . $image . '">
             </a>';
+        }
+    }
+    else {
+        echo 'dit soort films bieden wij aan op Fletnix';
     }
 ?>
 
