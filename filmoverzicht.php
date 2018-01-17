3e9d63aa295c0870c7f11337884fbac7125f00ca
Wout <w.hak99@gmail.com>
2018-01-17 19:47:47 +0100

    Filminformatie af, fixed footer bij inlog.php en inlog.php?=returnblabla

--- filmoverzicht.php
+++ filmoverzicht.php
@@ -8,6 +8,18 @@ require_once 'php/dbconnectie.php';
     <div class="index-container">
         <h1>Filmoverzicht</h1>
         <div class="index-item">
+            <form action="filmoverzicht.php" method="post">
+                <label for="titel">Zoeken op titel: </label>
+                <input type="text" id="titel" name="Zoeken op titel">
+                <label for="regisseur">Zoeken op regisseur: </label>
+                <input type="text" id="regisseur">
+                <label for="publicatiejaar">Zoeken op publicatiejaar: </label>
+                <input type="number" id="publicatiejaar" name="Zoeken op publicatiejaar">
+                <label for="zoeken"></label>
+                <input type="submit" id="zoeken" value="Zoeken">
+            </form>
+        </div>
+        <div class="index-item">
 <?php
     if (isset ($_SESSION['loginstatus']) && ($_SESSION['loginstatus'] == TRUE)) {
             $statement = "SELECT movie_id,cover_image FROM Movie";
